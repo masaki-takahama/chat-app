335d024aa230ff47c537414c8271b3895c9d198b
masaki-takahama <101110111+masaki-takahama@users.noreply.github.com>
2022-03-14 21:05:25 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
- root to: "messages#index"
+ root to: "rooms#index"
  resources :users, only: [:edit, :update]
  resources :rooms, only:[:new, :create]
 end
