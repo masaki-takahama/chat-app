5b28798481a4d32091ff84cc484a1922dcdf59f4
masaki-takahama <101110111+masaki-takahama@users.noreply.github.com>
2022-03-15 20:37:44 +0900

    チャットルームの削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
  root to: "rooms#index"
  resources :users, only: [:edit, :update]
- resources :rooms, only:[:new, :create] do
+ resources :rooms do
   resources :messages, only:[:index, :create]
  end
 end
