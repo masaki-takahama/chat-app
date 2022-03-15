0b57a3d70cb4df32b90d63468e901c7624b1d04d
masaki-takahama <101110111+masaki-takahama@users.noreply.github.com>
2022-03-15 12:39:25 +0900

    送信機能の実装

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,16 @@
 class Message < ApplicationRecord
+  belongs_to :user
+  belongs_to :room
+end
+
+def create
+  @room = Room.find(params[:room_id])
+  @message = @room.messages.new(message_params)
+  @message.save
+end
+
+private
+
+def message_params
+  params.require(:message).permit(:content).merge(user_id: current_user.id)
 end
